7d2f53d37b273612c72bcd1481b7b8ed8859d16e
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-09-26 19:34:50 +0200

    Update secureForm.php

--- frontend/secureForm.php
+++ frontend/secureForm.php
@@ -1,7 +1,7 @@
 <?php
 function secureForm($data)
         {
-            $sata=trim($data);
+            $data=trim($data);
             $data=stripslashes($data);
             $data=strip_tags($data);
             return $data;
